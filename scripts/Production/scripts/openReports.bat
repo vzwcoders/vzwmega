e656fc7407e9abbcefc5f39503ca038eb2e55b09
vzwcoders <vzwcoders@gmail.com>
2015-11-03 09:09:05 -0800

    scipt to open reports

---
+++ scripts/Production/scripts/openReports.bat
@@ -0,0 +1,6 @@
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome" http://localhost:8080/AlertingApp/sum.jsp?p=UDRDATPART3_2015_10_31_17_34_30
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome" http://localhost:8080/AlertingApp/sum.jsp?p=UDRDATPART1_2015_10_31_17_34_30
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome" http://localhost:8080/AlertingApp/alert.jsp
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome" http://localhost:8080/AlertingApp/alertReport.jsp
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome" http://localhost:8080/AlertingApp/ln.html
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome" http://localhost:8080/AlertingApp/alertSec.jsp
